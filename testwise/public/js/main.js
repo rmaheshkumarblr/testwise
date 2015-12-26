8e4383006e6e11fcc4b1ef71f201b7ad62c2317b
saurabhsood91 <saurabhsood91@gmail.com>
2015-12-26 12:32:51 -0700

    add login modal

--- testwise/public/js/main.js
+++ testwise/public/js/main.js
@@ -1,6 +1,22 @@
 "use strict"
 
 angular.module('TestWise', ['ui.bootstrap'])
-.controller('MainController', ['$scope', function($scope){
+.controller('MainController', ['$modal', function($modal){
   var self = this;
+  self.openLoginDialog = function() {
+    var modalInstance = $modal.open({
+      templateUrl: 'templates/login-modal.html',
+      controller: 'LoginModalInstanceCtrl as ctrl',
+      backdrop: 'static'
+    });
+  };
+}])
+.controller('LoginModalInstanceCtrl', ['$modalInstance',function ($modalInstance) {
+  var self = this;
+  self.login = function() {
+    // Login logic goes here
+  };
+  self.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
 }]);
