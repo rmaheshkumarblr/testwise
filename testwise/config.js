ba139273815cfb10aec1daf755a6d5045e580bc0
rmaheshkumarblr <rmaheshkumarblr@gmail.com>
2016-01-01 16:32:13 -0700

    Created a good template for questions creation - JSON created

--- testwise/config.js
+++ testwise/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  secret: '' //Add secret here - Do not commit
+  secret: 'TEST' //Add secret here - Do not commit
 };
--- testwise/public/js/controllers/AddTestController.js
+++ testwise/public/js/controllers/AddTestController.js
@@ -1,4 +1,32 @@
 angular.module('TestWise')
-.controller('AddTestController', function(){
+.controller('AddTestController', function()
+{
   var self = this;
+  self.values = {}
+  self.data = 
+  {
+  	availableOptions:
+  	[
+  	  {id: '0', name: 'Text'},
+      {id: '1', name: 'Choose One'},
+      {id: '2', name: 'Multi-Choice'},
+      {id: '3', name: 'Boolean'}
+    ]
+  }
+  self.print = function()
+  {
+  	console.log(self.values)
+  }
+  self.range = function(min,max,step)
+  	{
+  		// console.log("Called");
+  		step = step || 1;
+  		var input = [];
+  		for (var i = min; i <= max; i++) 
+  		{
+  			// console.log(i)
+  			input.push(i)
+  		}
+  		return input
+	};
 });
